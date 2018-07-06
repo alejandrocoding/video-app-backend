5cfc88b49f46f7083cbadf563550ab62fb615dc9
Alejandro <alejandrofpo@gmail.com>
2018-07-06 19:34:36 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'app.module';
 
 async function bootstrap() {
-  const port = 3000;
+  const port = process.env.PORT || 3333;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
   console.info(`http://localhost:${port}/`); // just for dev
+  console.log(process.env.ALEX);
 }
 
 bootstrap();
